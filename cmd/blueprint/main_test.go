9d345cb266abadf08ae6c61e6b28a063c5b94552
Siarhei Karnialiuk <sp_by@tut.by>
2022-07-14 19:51:18 +0300

    One more example test + Makefile fix.

--- cmd/blueprint/main_test.go
+++ cmd/blueprint/main_test.go
@@ -3,10 +3,18 @@ package main
 import (
 	"testing"
 
+	"github.com/serhijko/go-project-blueprint/cmd/blueprint/config"
 	"github.com/stretchr/testify/assert"
 )
 
 // Example test to show usage of `make test`
 func TestDummy(t *testing.T) {
 	assert.Equal(t, 1, 1)
+}
+
+func TestLoadConfig(t *testing.T) {
+	if err := config.LoadConfig("./config"); err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, "Dummy Value", config.Config.ConfigVar)
 }
