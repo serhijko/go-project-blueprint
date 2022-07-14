6ddc776dcd51455515d2afa9490b227f617f3990
Siarhei Karnialiuk <sp_by@tut.by>
2022-07-14 09:23:05 +0300

    Revert example test to first version.

--- cmd/blueprint/main_test.go
+++ cmd/blueprint/main_test.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/serhijko/go-project-blueprint/cmd/blueprint/config"
 	"github.com/stretchr/testify/assert"
 )
 
 // Example test to show usage of `make test`
 func TestDummy(t *testing.T) {
-	if err := config.LoadConfig("./config"); err != nil {
-		panic(fmt.Errorf("invalid application configuration: %s", err))
-	}
-	assert.Equal(t, "Dummy Value", config.Config.ConfigVar)
+	assert.Equal(t, 1, 1)
 }
